c9865e562ca94bba31f31d9712cddd74ba1421eb
Jake Wickstrom <jtwickstrom@alumni.ubc.ca>
2017-03-19 06:03:15 -0700

    First pass at scheduling calls

--- app/models.py
+++ app/models.py
@@ -8,14 +8,25 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     phone_number = models.CharField(max_length=15, unique=True)
+    last_completed_request = models.DateTimeField(default = datetime.datetime.now())
+    mmr = models.DecimalField(max_digits=2,decimal_places=1)
     active = models.BooleanField(default=False)
+    callFreq = models.DecimalField(default = 1)
+    pendingCallRequest = models.BooleanField(default = False)
 
 class Request(models.Model):
     requestee = models.ForeignKey(User, related_name='requests', on_delete=models.CASCADE)
     description = models.TextField()
     time_of_call = models.DateTimeField()
-    is_taken = models.BooleanField(default=False)
+    is_pending = models.BooleanField(default = False)
     is_completed = models.BooleanField(default=False)
+    completed_by = models.ForeignKey(User, related_name = 'completed_requests', on_delete=models.CASCADE)
+
+class allowedRequest(models.Model):
+    request = models.OneToOneField(Request)
+    allowed = models.BooleanField(default = False)
+
---
+++ app/scheduler.py
@@ -0,0 +1,59 @@
+
+from app.models import UserProfile, Request, allowedRequest
+import datetime
+from django.db.models.aggregates import Max
+from django.core import serializers
+from django.http import JsonResponse
+import math
+
+
+
+def getUserPriorities():
+    """ Returns a list of userIDs for all active users in order of their priority"""
+
+    activeUsers = UserProfile.entries.filter(active = True)
+
+    scoreTracker = {}
+    annotated_users = activeUsers.annotate(most_recent_call=Max('completed_requests__time_of_call'))
+    for user in activeUsers: 
+        #mostRecentCompletion = user.completedRequests.aggregate(Max('time_of_call'))['time_of_call__max']
+        mostRecentCompletition = user.most_recent_call
+        priorityScore = (datetime.datetime.now() - mostRecentCompletion).totalSeconds*user.callFreq
+        scoreTracker[priorityScore] = user
+
+    for score in sorted(scoreTracker.keys(),reversed = True):
+        priorityList.append(scoreTracker[score].id)
+
+    return priorityList
+
+def getBestRequest():
+    """Return the highest priority request in a JSON serialized format"""
+
+
+    earliestDeadline = activeRequests.aggregate(Min(time_of_call))[time_of_call__max]
+    bestRequest = activeRequests.get(time_of_call = earliestDeadline)
+
+    return bestRequest
+
+def getPermission(userID):
+    priorityList = getUserPriorities(UserProfile.entries.filter(active=True))
+
+    #if this user is in the top 20% of user priority
+    if userID in priorityList[:math.floor(len(priorityList)/5)]:
+        approvedRequest = getBestRequest()
+        user = UserProfile.entries.filter(user__id = userID)
+        data = {'permission_status' :True,
+                'description'       :user.description,
+                'requestee_number'  :approvedRequest.requestee.phone_number,
+                'request_id'        :approvedRequest.id}
+
+        #flag that this request has been passed to the user
+        approvedRequest.is_pending = True
+        approvedRequest.save()
+    else:
+        data = {'permission_status': False}
+    return JsonResponse(data)
+
+
+
+
